c36a9080bb8147daad0cbad57a8ad34fd412d31c
aaronli-uga <aaronli9281@gmail.com>
2022-02-01 16:59:44 -0500

    update

--- stepstone_attack/main_PC/scan_attack.sh
+++ stepstone_attack/main_PC/scan_attack.sh
@@ -3,9 +3,30 @@
 if [[ $# -lt 1 ]]; then
     echo 'Usage:'
     echo './scan_attack.sh command_code ip1 ip2 ip3 ...'
-    echo 'e.g. ./scan_attack.sh 192.168.0.2 192.168.0.5 192.168.0.11'
+    echo 'e.g. ./scan_attack.sh 5 192.168.0.2 192.168.0.5 192.168.0.11'
+    echo 'If no ip addresses given, default attack will be launched.'
     exit 1
 fi
 
-echo $#
-echo $1
+# Default attack mode.
+if [[ $# -eq 1 ]]; then
+    echo 'Default attack without specific ip addresses:'
+    ssh pi@10.42.0.187 > /dev/null 2>&1 << eeooff
+    cd Workspace
+    echo "$1" > test.txt
+    python publisher.py 1 "$1"
+    ./pi1.sh "$1" 
+    nc -zvn 10.42.0.187 1-36000
+    exit
+eeooff
+fi
+
+command=$1
+next_stepstone=$2
+ssh pi@$next_stepstone > /dev/null 2>&1 << eeooff
+cd Workspace
+echo "$2 $1" > test.txt
+exit 0
+eeooff 
+
+eeooff
