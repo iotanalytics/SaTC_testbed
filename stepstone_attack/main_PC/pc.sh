4e54f507b008fe3658af2409aead842ade44919f
aaronli-uga <aaronli9281@gmail.com>
2022-02-10 14:10:03 -0500

    delete unused.

--- stepstone_attack/main_PC/pc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ssh pi@10.42.0.187 > /dev/null 2>&1 << eeooff
-echo connected
-exit
-eeooff
-echo done!
--- stepstone_attack/main_PC/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# ssh pi@10.0.0.10 >/dev/null 2>&1 << eeooff
-# cd /home
-# ls
-# exit
-# eeooff
-# echo done!
-
-echo $@
